e8f85cf53ce1472c14a0081016793a1dbe19d3d3
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2024-06-29 15:06:52 -0400

    wip move emails to cronic

--- backup.sh
+++ backup.sh
@@ -2,20 +2,4 @@
 # PART 1: BACK IT UP
 
 source /opt/restic-scripts/.env
-
-SUBJECT="Restic: $( hostname ) Backup"
-OUTPUT=$( restic backup /home /opt --exclude-file=/opt/restic-scripts/excludes.txt 2>&1 )
-OUTPUT=${OUTPUT//$'\n'/\\n}
-
-curl --request POST \
-  --url https://api.sendgrid.com/v3/mail/send \
-  --header "Authorization: Bearer $SENDGRID_API_KEY" \
-  --header 'Content-Type: application/json' \
-  --data-binary @- <<EOF
-{
-  "personalizations": [{"to": [{"email": "$TO_EMAIL"}]}], 
-  "from": {"email": "$FROM_EMAIL"}, 
-  "subject": "$SUBJECT", 
-  "content": [{"type": "text/plain", "value": "$OUTPUT"}]
-}
-EOF
+restic backup /home /opt --exclude-file=/opt/restic-scripts/excludes.txt
--- cleanup.sh
+++ cleanup.sh
@@ -2,20 +2,4 @@
 # PART 3: CLEAN IT UP
 
 source /opt/restic-scripts/.env
-
-SUBJECT="Restic: Cleanup"
-OUTPUT=$( restic forget --prune --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75 2>&1 )
-OUTPUT=${OUTPUT//$'\n'/\\n}
-
-curl --request POST \
-  --url https://api.sendgrid.com/v3/mail/send \
-  --header "Authorization: Bearer $SENDGRID_API_KEY" \
-  --header 'Content-Type: application/json' \
-  --data-binary @- <<EOF
-{
-  "personalizations": [{"to": [{"email": "$TO_EMAIL"}]}], 
-  "from": {"email": "$FROM_EMAIL"}, 
-  "subject": "$SUBJECT", 
-  "content": [{"type": "text/plain", "value": "$OUTPUT"}]
-}
-EOF
+restic forget --prune --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75
